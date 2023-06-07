34b5cb9203af83eda4be94b96143e5694bcedbe6
Tom Richardson <firecracker37@gmail.com>
2023-06-07 17:44:01 -0500

    Sidebar mostly finished middleware implemented upgraded to Next Canary to fix an issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import Providers from '@/components/Providers'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Providers>{children}</Providers></body>
+      <body><Providers>{children}</Providers></body>
     </html>
   )
 }
